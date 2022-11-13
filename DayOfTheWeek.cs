147deec17089edf3918677d2f13eb4df5167221d
saskialorraine <104977190+saskialorraine@users.noreply.github.com>
2022-11-13 20:46:28 +0100

    Create DayOfTheWeek.cs
    Having the Bot greeting you with the current day of the week

---
+++ DayOfTheWeek.cs
@@ -0,0 +1,27 @@
+# SendActivity_EPfV8E_text()
+- Hi there!  😊
+- ```Heyy 🤗 What can I do for you?<p>
+Type "Menu" to get to the main menu.```
+- ```Hey! 🤭 <p>
+If you need an overview, check out the menu.<p>Simply type "Menu" and send. ```
+- Happy ${DayOfWeek()}!
+
+#DayOfWeek()
+- SWITCH: ${dayOfWeek(utcNow())}
+- CASE: ${1}
+    - Monday
+- CASE: ${2}
+    - Tuesday
+- CASE: ${3}
+    - Wednesday
+- CASE: ${4}
+    - Thursday
+- CASE: ${5}
+    - Friday
+- CASE: ${6}
+    - Saturday
+-CASE: ${7}
+    - Saturday
+- DEFAULT:
+    - What
+    
